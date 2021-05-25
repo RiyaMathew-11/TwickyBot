d7c3dc5b908186b07043195c02b4ab363e607624
Riya P Mathew <riyapmathew2000@gmail.com>
2021-05-25 12:38:19 +0530

    Deployment commit

--- threadsaver_bot.py
+++ threadsaver_bot.py
@@ -10,10 +10,10 @@ logger = logging.getLogger()
 
 
 def create_api():
-    consumer_key = os.environ('CONSUMER_KEY')
-    consumer_secret = os.environ('CONSUMER_SECRET')
-    access_token = os.environ('ACCESS_TOKEN')
-    access_token_secret = os.evviron('ACCESS_TOKEN_SECRET')
+    consumer_key = os.environ['CONSUMER_KEY']
+    consumer_secret = os.environ['CONSUMER_SECRET']
+    access_token = os.environ['ACCESS_TOKEN']
+    access_token_secret = os.evviron['ACCESS_TOKEN_SECRET']
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
